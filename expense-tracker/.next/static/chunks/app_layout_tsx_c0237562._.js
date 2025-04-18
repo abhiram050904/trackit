c45b6cce92acdabdf4cf2a2f1a884b90dbe6bf24
ABHIRAM GATTAMNENI <abhiram050904@gmail.com>
2025-04-18 21:26:57 +0530

    landing-page

--- expense-tracker/.next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__608acd83._.css",
-  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_1702e59e._.js",
-  "static/chunks/node_modules_637f2f8c._.js"
-],
-    source: "dynamic"
-});
